cdb23f99a9ce4de79e7cb98dccdfeed3a106896b
nzandy <andydaviesnz@gmail.com>
2017-08-02 08:31:10 +0100

    Initial checkin

---
+++ TransactionWebApi/TransactionWebApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TransactionWebApi {
+	public class Program {
+		public static void Main(string[] args) {
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.UseApplicationInsights()
+				.Build();
+
+			host.Run();
+		}
+	}
+}
