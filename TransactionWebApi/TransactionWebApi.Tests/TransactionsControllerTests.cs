7136089a3a61ad042561fdf2e8cdaee6abf1b713
nzandy <andydaviesnz@gmail.com>
2017-08-02 15:35:24 +0100

    Add first test for get transactions.

--- TransactionWebApi/TransactionWebApi.Tests/TransactionsControllerTests.cs
+++ TransactionWebApi/TransactionWebApi.Tests/TransactionsControllerTests.cs
@@ -6,6 +6,7 @@ using TransactionWebApi.Controllers;
 using TransactionWebApi.Tests.Mocks;
 using TransactionWebApi.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TransactionWebApi.Tests {
 	[TestClass]
@@ -17,7 +18,16 @@ namespace TransactionWebApi.Tests {
 		public void Setup() {
 			ITransactionRepository _repository = new MockTransactionRepository();
 			_controller = new TransactionsController(_repository);
+		}
 
+		[TestMethod]
+		public void GetAllReturnsAllTransactions() {
+			IActionResult result = _controller.Get();
+			OkObjectResult objectResult = result as OkObjectResult;
+			Assert.IsNotNull(objectResult);
+			List<Transaction> responseTransactions = objectResult.Value as List<Transaction>;
+			Assert.IsNotNull(responseTransactions);
+			Assert.AreEqual(2, responseTransactions.Count);
 		}
 	}
 }
